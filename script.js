c922d1d5af002294badf472b29f0784d80a1ecbf
Draike13 <tmbiri91@gmail.com>
2025-02-01 13:40:12 -0600

    fucntional delete and box

--- script.js
+++ script.js
@@ -6,23 +6,22 @@ class ListItem {
   }
 }
 
-function formSubmit() {
-  console.log(item.value);
-  console.log(quant.value);
-}
-
 let holder = document.getElementById('rowHolder');
 
 function createRow() {
   let row = new ListItem(item.value, quant.value);
   let newDiv = document.createElement('div');
+  //   let quantInput = document.createElement('select');
+  //   quantInput.value = row.quantity;
   row.deleteButton.textContent = 'Delete Me';
   row.deleteButton.addEventListener('click', () => {
     newDiv.remove();
     row.deleteButton.remove();
   });
   console.log(row.deleteButton);
-  newDiv.innerHTML = row.item + row.quantity;
+  newDiv.innerHTML = row.item;
+  newDiv.append(row.quantity);
   newDiv.append(row.deleteButton);
   holder.appendChild(newDiv);
+  console.log(holder);
 }
